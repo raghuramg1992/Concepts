6865e6aaecca44bee7c639b15da5ce8532225b7f
Raghuram G <raghuramg1992@gmail.com>
2020-09-03 01:18:33 +0530

    HeightoFTree

--- Concepts/src/DataStructure/HeightOfTree.java
+++ Concepts/src/DataStructure/HeightOfTree.java
@@ -18,12 +18,21 @@ class Node {
 public class HeightOfTree {
 
 	public static int height(Node root) {
-      	// Write your code here.
-          int length=0;
-          
-         return length;
 
-    }
+	    int leftHeight = 0;
+	    int rightHeight = 0;
+
+	    if (root.left != null) {
+	        leftHeight = 1 + height(root.left);
+	    }
+
+	    if (root.right != null) {
+	        rightHeight = 1 + height(root.right);
+	    }
+
+	    return leftHeight > rightHeight ? leftHeight : rightHeight;
+	}
+
 
 	public static Node insert(Node root, int data) {
         if(root == null) {
